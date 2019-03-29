b81fc199c20ce832c0f530dcf6cd56f7c667efb5
Mikkel Glerup <Mikkel Glerup@MIKKELGLERUP>
2019-03-29 14:30:30 +0100

    Added GameManager prefab and script
    Started GameManager
    Added
    LapGoal int
    Array of RacerUnit (cannot complete until RacerUnit is complete)
    CurrentRacer int

---
+++ UltimateAdventureRacing/Assets/Scripts/GameManagerLogic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerLogic : MonoBehaviour
+{
+    public int lapGoal;
+    //public Racers[] RacerUnit;
+    private int CurrentRacer; //Who's turn it is
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
