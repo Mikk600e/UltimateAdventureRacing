37e0689751a220f36b80b1d4ddecd1fc51e4bd4f
KlausKristensen <Klaus@DESKTOP-CEDJEJ0>
2019-03-29 14:08:27 +0100

    Made Field
    Created a simple script for field and just a simple prefab

---
+++ UltimateAdventureRacing/Assets/Scripts/scr_Field.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_Field : MonoBehaviour
+{
+    public int section;
+    public int lane;
+    public int placementNumber;
+    public bool isLapFinish;
+    public bool isLapStart;
+    public bool isSectionFinish;
+    public bool isSectionStart;
+}
